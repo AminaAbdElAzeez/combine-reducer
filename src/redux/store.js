90419a2b47a46b80a53be763a694c6c409a83b17
AminaAbdElAzeez <aminaabdelazeez99@gmail.com>
2023-06-10 14:48:58 +0300

    combine reducer

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,13 +1,11 @@
-import {applyMiddleware, configureStore} from '@reduxjs/toolkit'
-import reduxThunk from 'redux-thunk'
+import { configureStore} from '@reduxjs/toolkit'
 import rightSlice from './rightSlice';
 import leftSlice from './leftSlice';
 
-const enhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__;
 const store = configureStore({
     reducer : {
         left : leftSlice,
         right : rightSlice
     }
-},enhancer(applyMiddleware(reduxThunk)))
+})
 export default store;
